54b5470823432237e880f0e0bb88da4d48b554f1
Sakshijadhav77 <sakshijadhav277@gmail.com>
2023-05-16 10:08:17 +0530

    E-Patrakaar

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB paddingTB'>
+          <div>
+            <h1>E-Patrakaar</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
